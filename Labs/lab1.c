0f76c22f70f87548ada0e4d06fb22205c1ef91e8
Julia <berrylily.0709@gmail.com>
2017-08-02 10:12:53 -0700

    Add lab1 files

---
+++ Labs/lab1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main() {
+
+	// 1. Decimal to Binary
+	// a. 12 -> 1100
+	// b. 185 -> 10111001
+	// c. 294 -> 100100110
+	//
+	// 2. Binary to Decimal
+	// a. 101101 -> 45
+	// b. 11101011 -> 235
+	// c. 1010 -> 10
+	//
+	// 3. Decimal to Hexadecimal
+	// a. 138 -> 0x8A
+	// b. 119 -> 0x77
+	// c. 59 -> 0x3B
+	//
+	// 4. Hexadecimal to Decimal
+	// a. 0x1A -> 26
+	// b. 0x3F31 -> 337
+	// c. 0x31C -> 76
+	//
+	// 5.Binary to Hexadecimal
+	// a. 10010111 -> 0x95
+	// b. 101101111110 -> 0xB80
+	// c. 1111 -> 0x0F
+	//
+	// 6. Hexadecimal to Binary
+	// a. 0x1A3B -> 1101000110000
+	// b. 0xABCD -> 1010101111001101
+	// c. 0x111111 -> 100010001000100010001
+	
+	printf("compile succeeded!\n");
+	return 0;
+}
