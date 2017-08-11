c73e635e0a8b5a5c95b4ea5a20397f0c6eb6a723
Julia <berrylily.0709@gmail.com>
2017-08-11 11:49:30 -0700

    Add Ex4 - 6 in lab3

--- Labs/Lab3/lab3.c
+++ Labs/Lab3/lab3.c
@@ -6,6 +6,22 @@ void swap(int *p1, int *p2) {
 	*p2 =tmp;
 }
 
+void print_addr(int x) {
+	printf("Variable address is: %p \n", (void*)&x);
+}
+
+int *new_integer() {
+	int n  = 123;
+	int *addr = &n;
+	return addr;
+}
+
+void print_array(int arr[], int l) {
+	for(int i = 0; i < l; i++) {
+		printf("arr[%d]: %d \n", i, *(arr + i));
+	}
+}
+
 int main() {
 	// 1. Swap values
 	int x = 10, y = 20;
@@ -23,6 +39,20 @@ int main() {
 	for(int i = 0; i < sizeof(arr) / sizeof(*pa); i++) {
 		printf("arr[%d]: %d \n", i, *(arr + i));
 	}
+
+	// 4. Print a variable's address
+	int v = 1111;
+	printf("Variable address is: %p \n", &v);
+	print_addr(v);
+
+	// 5. Declare and initialize an integer in function
+	// The function will point the above v address and override the value v
+	printf("Address is: %p \n", new_integer());
+	printf("v: %d \n", *new_integer());
+
+	// 6. Print all vallues of an integer array
+	int l = sizeof(arr) / sizeof(arr[0]);
+	print_array(arr, l);
 
 	return 0;
 }
