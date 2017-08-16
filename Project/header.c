1886f838e0b8845bfc086bfe26b4dbc44c7fc26f
Julia <berrylily.0709@gmail.com>
2017-08-16 15:39:11 -0700

    Display date when file opens

--- Project/header.c
+++ Project/header.c
@@ -8,15 +8,19 @@
 void open_file(char *filename) {
 	FILE *fp = fopen(filename, "r");
 
-	char date[MAX] = {'\0'};
-	time_t t = time(NULL);
-	struct tm *timep = localtime(&t);
-	strftime(date, MAX, "%a %b %d %T %Z %Y", timep);
+	if(fp) {
+		char date[MAX] = {'\0'};
+		time_t t = time(NULL);
+		struct tm *timep = localtime(&t);
+		strftime(date, MAX, "%a %b %d %T %Z %Y", timep);
 
-	printf("started on %s \n", date);
-	printf("Input file: %s \n", filename);
+		printf("started on %s \n", date);
+		printf("Input file: %s \n", filename);
 
-	fclose(fp);
+		fclose(fp);
+	} else {
+		printf("failed to open %s \n", filename);
+	}
 }
 
 void readUntilMarker(char marker) {
