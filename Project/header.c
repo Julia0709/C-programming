7dd038fc1ac3d47736aff931c0336c05e5fd5dbe
Julia <berrylily.0709@gmail.com>
2017-08-16 08:54:59 -0700

    Display file opening time

--- Project/header.c
+++ Project/header.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <time.h>
 #include "header.h"
+
+#define MAX 256
 
 void readEntireFile(void) {
 	FILE *fp = fopen("inputfile1.txt", "r");
 
-	int c = getc(fp);
-	while(c != EOF) {
-		putchar(c);
-		c = getc(fp);
-	}
+	char date[MAX] = {'\0'};
+
+	time_t t = time(NULL);
+	struct tm *timep = localtime(&t);
+	strftime(date, MAX, "%a %b %d %T %Z %Y", timep);
+
+	printf("started on %s \n", date);
+	printf("Input file: inputfile1.txt \n");
+
+//	int c = getc(fp);
+//	while(c != EOF) {
+//		putchar(c);
+//		c = getc(fp);
+//	}
 
 	fclose(fp);
 }
 
 void readUntilMarker(char marker) {
+	FILE *fp = fopen("inputfile1.txt", "r");
 
+	int c = getc(fp);
+	while(c != marker) {
+		putchar(c);
+		c = getc(fp);
+	}
+
+	fclose(fp);
 }
 
 void writeToFile(void) {
