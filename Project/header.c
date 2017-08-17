50e16a8bed85c3585ec65e6d301827a5260c2f51
Julia <berrylily.0709@gmail.com>
2017-08-16 22:29:10 -0700

    Print result

--- Project/header.c
+++ Project/header.c
@@ -9,18 +9,19 @@
 struct counter {
 	char word[MAX];
 	int count;
+	char message[MAX];
 	struct counter *next;
 };
 
 static struct counter list[] = {
-	{"Image", 0},
-	{"lineTo", 0},
-	{"End", 0},
-	{"print", 0},
-	{"draw", 0},
-	{"translate", 0},
-	{"child", 0},
-	{"#", 0},
+	{"Image", 0, "Image definition(s)"},
+	{"lineTo", 0, "lineTo command(s) within Figures"},
+	{"End", 0, "End command(s)"},
+	{"print", 0, "print command(s)"},
+	{"draw", 0, "draw command(s)"},
+	{"translate", 0, "translate command(s)"},
+	{"child", 0, "child command(s)"},
+	{"#", 0, "comment(s)"},
 };
 
 // Define nexts for linked list
@@ -36,7 +37,8 @@ struct counter *define_next() {
 // Print count result
 void print_result(struct counter *ptr) {
 	while(ptr != NULL) {
-		printf("word: %s  count: %d \n", ptr->word, ptr->count);
+		printf("%d %s \n", ptr->count, ptr->message);
+		ptr = ptr->next;
 	}
 }
 
@@ -45,7 +47,7 @@ int count_word(struct counter *ptr, char *token) {
 	// Define linked list node
 	struct counter *head = define_next();
 
-	while (ptr != NULL) {
+	while(ptr != NULL) {
 		if(strcmp(ptr->word, token) == 0) {
 			ptr->count++;
 			printf("word: %s \n", ptr->word);
@@ -58,7 +60,7 @@ int count_word(struct counter *ptr, char *token) {
 }
 
 // Read input file and convert to string
-char *read_file(FILE *fp) {
+int read_file(FILE *fp) {
 	// Check the file size
 	fseek(fp, 0, SEEK_END);
 	long fsize = ftell(fp);
@@ -67,17 +69,32 @@ char *read_file(FILE *fp) {
 	char *str = malloc(fsize + 1);
 	fread(str, fsize, 1, fp);
 
-	const char s[2] = " ";
-	char *token = strtok(str, s);
-	printf("token1: %s \n", token);
+	const char L[2] = "\n";
+	char *line = strtok(str, L);
+//	while(line != NULL) {
+//		const char S[2] = " ";
+//		char *token = strtok(str, S);
+//		printf("token1: %s \n", token);
 
-	count_word(list, token);
-// 	while( token != NULL ) {
-//		printf( " %s\n", token );
-//		token = strtok(NULL, s);
+
+//		char *line = strtok(str, L);
+//		token = strtok(NULL, S);
+//		line = strtok(NULL, L);
 //	}
 
-	return token;
+ 	while(line != NULL) {
+		printf("%s \n", line);
+
+//		const char S[2] = " ";
+//		char *token = strtok(line, S);
+//		printf("token: %s \n", token);
+
+		count_word(list, line);
+		line = strtok(NULL, L);
+	}
+
+	print_result(list);
+	return 0;
 }
 
 // Open file and display time
